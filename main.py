aa90f24de3d25fc062e7708a73b17bb483edc1b9
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-02-16 05:13:30 +0000

    Added comments of the process

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-# 🚨 Don't change the code below 👇
+
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+combined_name = name1 + name2
 
-combined_names = name1 + name2
-lower_names = combined_names.lower()
-t = lower_names.count("t")
-r = lower_names.count("r")
-u = lower_names.count("u")
-e = lower_names.count("e")
-first_digit = t + r + u + e
+# converting to lower case input value
+lower_case = combined_name.lower()
+#counting each occurance of true love with the input
+t = lower_case.count("t")
+r = lower_case.count("r")
+u = lower_case.count("u")
+e = lower_case.count("e")
 
-l = lower_names.count("l")
-o = lower_names.count("o")
-v = lower_names.count("v")
-e = lower_names.count("e")
-second_digit = l + o + v + e
+true = t + r + u + e
 
-score = int(str(first_digit) + str(second_digit))
+l = lower_case.count("l")
+o = lower_case.count("o")
+v = lower_case.count("v")
+e = lower_case.count("e")
 
-if (score < 10) or (score > 90):
-  print(f"Your score is {score}, you go together like coke and mentos.")
-elif (score >= 40) and (score <= 50):
-  print(f"Your score is {score}, you are alright together.")
+love = l + o + v + e
+
+true_love_score = int(str(true) + str(love))
+
+print(true_love_score)
+
+#logic
+if (true_love_score < 10) or (true_love_score > 90) :
+    print(f"Your score is {true_love_score}, you go together like coke and mentos.")
+elif (true_love_score >= 40) and (true_love_score <= 50):
+    print(f"Your score is {true_love_score}, you are alright together.")
 else:
-  print(f"Your score is {score}.")
+    print(f"Your score is {true_love_score}")
+
 
 
 
