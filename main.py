4ac328d1e64df4dada66b71de7222d57b1b11c7d
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-04-02 20:06:15 -0500

    Added to the machine

--- main.py
+++ main.py
@@ -7,30 +7,28 @@ combined_name = name1 + name2
 # converting to lower case input value
 lower_case = combined_name.lower()
 
-#counting each occurance of true love with the input
+# counting each occurance of true love with the input
 t = lower_case.count("t")
 r = lower_case.count("r")
 u = lower_case.count("u")
 e = lower_case.count("e")
-
 true = t + r + u + e
 
 l = lower_case.count("l")
 o = lower_case.count("o")
 v = lower_case.count("v")
 e = lower_case.count("e")
-
 love = l + o + v + e
 
 true_love_score = int(str(true) + str(love))
 
 print(true_love_score)
 
-#logic
-if (true_love_score < 10) or (true_love_score > 90) :
-    print(f"Your score is {true_love_score}, you go together like coke and mentos.")
+# logic
+if (true_love_score < 10) or (true_love_score > 90):
+    print(
+        f"Your score is {true_love_score}, you go together like coke and mentos.")
 elif (true_love_score >= 40) and (true_love_score <= 50):
     print(f"Your score is {true_love_score}, you are alright together.")
 else:
     print(f"Your score is {true_love_score}")
-
